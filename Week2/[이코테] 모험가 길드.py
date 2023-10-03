3b2d09fcba4337a3740fe3faa95762afe0019c34
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-10-04 00:25:22 +0900

    Create [이코테] 모험가 길드.py

---
+++ Week2/[이코테] 모험가 길드.py
@@ -0,0 +1,12 @@
+def solution(data):
+    result = 0
+    count = 0
+    
+    for i in data:
+        count += 1
+        if count >= i:
+            result += 1
+            count = 0
+        
+    return result
+
