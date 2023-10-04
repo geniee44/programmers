6352d9cc72db7b6036bc4f175ced6db989c86cfb
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-10-05 03:10:46 +0900

    Create [이코테] 왕실의 나이트.py

---
+++ Week2/[이코테] 왕실의 나이트.py
@@ -0,0 +1,15 @@
+def solution(row, col):
+    # 완전 탐색
+    steps = [(-1, -2). (-1, 2), (1, -2), (1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1)]
+    result = 0
+    r = row
+    c = int(ord(col) - int(ord('a')) + 1
+    
+    for step in steps:
+        new_row = r + step[0]
+        new_col = c + step[1]
+        
+        if 1 <= new_row <= 8 and 1 <= new_col <= 8:
+            result += 1
+    
+    return result
