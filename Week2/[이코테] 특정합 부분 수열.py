c16190c9954bb0b1fde2f98425fa1e01a5c620cf
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-10-06 20:33:05 +0900

    Create [이코테] 특정합 부분 수열.py

---
+++ Week2/[이코테] 특정합 부분 수열.py
@@ -0,0 +1,19 @@
+import sys
+n, m = map(int, input().split(' '))
+
+data = list(map(int, input().split(' ')))
+count = 0
+interval_sum = 0
+
+
+for start in range(n):
+    while interval_sum < m and end < n:
+        interval_sum += data[end]
+        end += 1
+    
+    if interval_sum == m:
+        count += 1
+    
+    interval_sum -= data[start]
+    
+print(count)
