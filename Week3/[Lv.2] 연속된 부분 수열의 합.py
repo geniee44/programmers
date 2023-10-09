ca1bc664f201bad9cf75180197a705a9711fa0c1
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-10-09 22:32:49 +0900

    Create [Lv.2] 연속된 부분 수열의 합.py

---
+++ Week3/[Lv.2] 연속된 부분 수열의 합.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def solution(sequence, k):
+    answer = []
+    
+    candidate = []
+    q = deque()
+    q.append(sequence[0])
+    i = 1
+    
+    while True:
+        if sum(q) > k:
+            if len(q) == 1:
+                i += 1
+                q.append(sequence[i])
+            q.popleft()
+        elif sum(q) < k:
+            i += 1
+            if i == len(sequence):
+                break
+            q.append(sequence[i])
+        else:
+            candidate.append((len[q], i)
+            q.popleft()
+
+    candidate.sort()
+    answer.append(candidate[0][1] - candidate[0][0])
+    answer.append(candidate[0][1])
+                             
+    return answer
