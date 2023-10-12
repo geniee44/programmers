60e439ddf65c00bdee901154eeb5e36c4dfd2637
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-10-13 00:05:42 +0900

    Update [Lv.2] 과제 진행하기.py

--- Week3/[Lv.2] 과제 진행하기.py
+++ Week3/[Lv.2] 과제 진행하기.py
@@ -18,14 +18,19 @@ def solution(plans):
             pause.append((i, int(plans[i][2]) - start_list[i + 1] + start_list[i]))
         else:
             answer.append(plans[i][0])
+            now += int(plans[i][2])
             while len(pause) > 0:
                 index, time = pause.pop()
-                if now + int(plans[i][2]) + time == start_list[i + 1]:
+                if now + time == start_list[i + 1]:
                     answer.append(plans[index][0])
                     break
                 elif now + int(plans[i][2]) + time > start_list[i + 1]:
-                    rest = time - (start_list[i + 1] - now - int(plans[i][2]))
+                    rest = time - (start_list[i + 1] - now)
                     pause.append((index, rest))
+                    break
+                else:
+                    now += plans[index][2]
+                    answer.append(plans[index][0])
 
     answer.append(plans[-1][0])
 
@@ -34,4 +39,3 @@ def solution(plans):
         answer.append(plans[index][0])
     
     return answer
-  
