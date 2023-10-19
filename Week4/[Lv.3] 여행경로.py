0244888282c8a29fd6ec7739919be08ffa2aec1f
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-10-19 19:54:41 +0900

    Create [Lv.3] 여행경로.py

---
+++ Week4/[Lv.3] 여행경로.py
@@ -0,0 +1,29 @@
+def solution(tickets):
+    answer = []
+    tickets = sorted(tickets)
+    countries = []
+    visited = [False for _ in range(len(tickets))]
+
+    def find_tickets(country):
+        for i in range(len(tickets)):
+            if not visited[i] and tickets[i][0] == country:
+                return i
+        return -1
+
+    idx = find_tickets("ICN")
+    visited[idx] = True
+    answer.append("ICN")
+    countries.append(tickets[idx][1])
+
+    while countries:
+        now = countries.pop()
+        idx = find_tickets(now)
+        if idx == -1:
+            answer.append(now)
+            break
+        visited[idx] = True
+        countries.append(tickets[idx][1])
+
+        answer.append(now)
+
+    return answer
