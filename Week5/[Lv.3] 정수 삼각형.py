dc8ea062c8a46fd153be86204040ea0ba3a646c9
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-10-25 20:11:21 +0900

    Update [Lv.3] 정수 삼각형.py

--- Week5/[Lv.3] 정수 삼각형.py
+++ Week5/[Lv.3] 정수 삼각형.py
@@ -1,20 +1,17 @@
 from collections import deque
 def solution(triangle):
     answer = 0
-    n = sum([i for i in range(1, len(triangle) + 1)])
     dp = [[0] * i for i in range(1, len(triangle) + 1)]
-    q = deque()
-    q.append((0, 0, 0))
+    dp[0][0] = triangle[0][0]
     
-    while q:
-        idx, loc, w = q.popleft()
-        dp[idx][loc] = max(dp[idx][loc], w + triangle[idx][loc])
-        
-        if idx == len(triangle) - 1:
-            continue
-                
-        if 0 <= loc < len(triangle[idx]):
-            q.append((idx + 1, loc + 1, dp[idx][loc]))
-            q.append((idx + 1, loc, dp[idx][loc]))
-        
+    for x in range(1, len(triangle)):
+        for y in range(len(triangle[x])):
+            if y == 0:
+                dp[x][y] = dp[x - 1][y] + triangle[x][y]
+            elif y == len(triangle[x]) - 1:
+                dp[x][y] = dp[x - 1][y - 1] + triangle[x][y]
+            else:
+                dp[x][y] = max(dp[x - 1][y - 1], dp[x - 1][y]) + triangle[x][y]
+
     return max(dp[len(triangle) - 1])
+
